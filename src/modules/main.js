453095bc353e965f278f96e831c9d2afa0fbef04
Romserg <serg.romankov@gmail.com>
2017-08-15 12:50:53 +0300

    all done

--- src/modules/main.js
+++ src/modules/main.js
@@ -1,6 +1,15 @@
 import $ from 'jquery'; // eslint-disable-line
 export default {};
 
+function compareName(a, b) { // eslint-disable-line
+	if (a.name.first > b.name.first) return 1;
+	if (a.name.first < b.name.first) return -1;
+}
+
+function compareNumeric(a, b) { // eslint-disable-line
+	if (a.age > b.age) return 1;
+	if (a.age < b.age) return -1;
+}
 
 $(document).ready(() => {
 	$.ajax('http://www.mocky.io/v2/55f748b33568195d044b3dc8').done((response) => {
@@ -34,13 +43,35 @@ $(document).ready(() => {
 			}
 			return {};
 		});
-		response.filter((elem) => {
-			const array = [];
-			array.push(elem.name.first);
-			array.sort();
-			// console.log(array);
-			return {};
-		});
+		response.sort(compareNumeric).forEach(elem =>
+			$('.age-sorted')
+				.append(`<tr><td><img src='${elem.picture}'></td>
+						 <td>${elem.name.first} ${elem.name.last}</td>
+						 <td>${elem.isActive}</td>
+						 <td>${elem.about}</td>
+						 <td>${elem.balance}</td>
+						 <td>${elem.age}</td>
+						 <td>${elem.registered}</td>
+						 <td>${elem.company}</td>
+						 <td><a href='mailto:${elem.email}'>${elem.email}</a></td>
+						 <td><a href='tel:${elem.phone}'>${elem.phone}</a></td>
+						 <td>${elem.address}</td></tr>`)
+		);
+		response.sort(compareName).forEach(elem =>
+			$('.fullname-sorted')
+				.append(`<tr><td><img src='${elem.picture}'></td>
+						 <td>${elem.name.first} ${elem.name.last}</td>
+						 <td>${elem.isActive}</td>
+						 <td>${elem.about}</td>
+						 <td>${elem.balance}</td>
+						 <td>${elem.age}</td>
+						 <td>${elem.registered}</td>
+						 <td>${elem.company}</td>
+						 <td><a href='mailto:${elem.email}'>${elem.email}</a></td>
+						 <td><a href='tel:${elem.phone}'>${elem.phone}</a></td>
+						 <td>${elem.address}</td></tr>`)
+		);
+
 		response.filter((elem) => {
 			if (elem.name.last.length >= 6) {
 				$('.lastname-more-six')
